c50ae7247bfc32eec03cdf33578860ade79fe0fb
appoclayptor <shahreza1414@gmail.com>
2024-11-15 11:39:58 +0600

    registering the user with firebase completed also added onauthchanged observer

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,36 +1,67 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Register = () => {
+
+    const { createNewUser, setUser } = useContext(AuthContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Get Form data
+        const form = new FormData(e.target);
+        const name = form.get('name');
+        const email = form.get('email')
+        const photo = form.get('photo')
+        const password = form.get('password')
+        console.log({ name, email, photo, password });
+
+
+        createNewUser(email, password)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+    }
+
+
+
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
                 <h2 className="text-2xl font-semibold text-center">Register Your Account</h2>
-                <form className="card-body">
+                <form onSubmit={handleSubmit} className="card-body">
                     <div className="form-control font-semibold">
                         <label className="label">
                             <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="name" className="input input-bordered" required />
+                        <input name="name" type="text" placeholder="name" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Photo URL</span>
                         </label>
-                        <input type="text" placeholder="photo url" className="input input-bordered" required />
+                        <input type="text" name="photo" placeholder="photo url" className="input input-bordered" required />
                     </div>
                     {/* Email Input */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                     </div>
                     {/* Password input */}
                     <div className="form-control">
                         <label className="label">
                             <span className="label-text font-semibold">Password</span>
                         </label>
-                        <input type="password" placeholder="password" className="input input-bordered" required />
+                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                         <label className="label">
                             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
